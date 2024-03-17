7b2039858f2c048b96c0dbe72f75092176886141
圣 <bawfgfhv@qq.com>
2024-03-17 21:28:34 +0800

    Opiddict++

---
+++ DaprIdentity/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using DaprIdentity.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DaprIdentity.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    /// <inheritdoc />
+    public ApplicationDbContext(DbContextOptions options)
+        : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder builder) { }
+}
